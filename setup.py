d28db852977bb35154ee81b616d7fa9e207c662b
Felipe Linhares <102616725+linhares06@users.noreply.github.com>
2024-06-12 19:02:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=page_description,
     long_description_content_type='text/markdown',
     author='Felipe',
-    url='https://github.com/yourusername/text-progress-bar',
+    url='https://github.com/linhares06/dio-desafio-projeto-criacao-pacotes',
     packages=find_packages(),
     python_requires='>=3.6',
 )
